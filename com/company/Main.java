c572e0c87d6c580bb35d7f6360c1b58aa21f62b2
David-tech65 <77960146+David-tech65@users.noreply.github.com>
2021-06-18 20:20:47 +0300

    try to do rebase

--- com/company/Main.java
+++ com/company/Main.java
@@ -16,9 +16,13 @@ public class Main {
         System.out.println("Make first feature more interesting");
         System.out.println("I've made the first feature");
 
+
         second_feature a = new second_feature();
 
         System.out.println("Going on do master project ,Amigo");
+
+
+        System.out.println("Try to do second feature");
 
 
 
