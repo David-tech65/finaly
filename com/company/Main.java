dc18f60ffa54358754307a615c30bb0cdc41dd8e
David-tech65 <77960146+David-tech65@users.noreply.github.com>
2021-06-18 19:53:34 +0300

    ff v0.6

--- com/company/Main.java
+++ com/company/Main.java
@@ -8,5 +8,8 @@ public class Main {
         System.out.println("In commit m v0.2");
 
         System.out.println("Make first feature more interesting");
+        System.out.println("I've made the first feature");
+
+
     }
 }
